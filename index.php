652038ebdffb4877da4b53640f8c884b0dc88d40
Chingupingu30 <teodavid2994@gmail.com>
2026-05-04 14:57:24 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <html>
 
 <head>
-  <!-- Histats.com  START  (aync)-->
+    <!-- Histats.com  START  (aync)-->
 <script type="text/javascript">var _Hasync= _Hasync|| [];
-_Hasync.push(['Histats.start', '1,5023999,4,0,0,0,00010000']);
+_Hasync.push(['Histats.start', '1,5024628,4,0,0,0,00010000']);
 _Hasync.push(['Histats.fasi', '1']);
 _Hasync.push(['Histats.track_hits', '']);
 (function() {
@@ -12,7 +12,7 @@ var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async
 hs.src = ('//s10.histats.com/js15_as.js');
 (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
 })();</script>
-<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?5023999&101" alt="html hit counter" border="0"></a></noscript>
+<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?5024628&101" alt="free geoip" border="0"></a></noscript>
 <!-- Histats.com  END  -->
   <!-- Basic -->
   <meta charset="utf-8" />
